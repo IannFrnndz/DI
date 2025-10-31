ac884fa92e8aa9e9745de0ec028f24a0e7bc3c37
Ian Fernández Gamo <ian.fernandezgamo24@campusfp.es>
2025-10-31 10:45:50 +0100

    Add Bootstrap integration and enhance form functionality

---
+++ Bootstrap/index.js
@@ -0,0 +1,19 @@
+let formulario = document.getElementById("formulario");
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+let enviar = document.getElementById("enviar");
+
+console.log(email, password, enviar);
+
+enviar.addEventListener("click", (e) =>{
+    e.preventDefault();
+    console.log(email.value);
+    console.log(password.value);
+
+    if(password.value == 123){
+        alert("Mario calla");
+        document.body.style.background= "red";
+    }else{
+        document.body.style.background = "green";
+    }
+})
