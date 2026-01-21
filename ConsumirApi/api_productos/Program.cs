9a17784bf9b600fcce74d964c895f4b8e90ae763
Ian Fernández Gamo <ian.fernandezgamo24@campusfp.es>
2026-01-21 13:16:45 +0100

    Actualizacion dia 21 enero

--- ConsumirApi/api_productos/Program.cs
+++ ConsumirApi/api_productos/Program.cs
@@ -1,18 +1,18 @@
-﻿var constructor = WebApplication.CreateBuilder(args);
+﻿// el constructor
+var constructor =  WebApplication.CreateBuilder(args);
 
-// CORS
 constructor.Services.AddCors(opciones =>
 {
-    opciones.AddDefaultPolicy(politica =>
-        politica.WithOrigins("http://127.0.0.1:5500", "http://localhost:5500")
+    opciones.AddDefaultPolicy(politicas =>
+        politicas.WithOrigins("http://127.0.0.1:5500", "http://127.0.0.1:5500")
                 .AllowAnyHeader()
                 .AllowAnyMethod());
+ 
 });
 
 var app = constructor.Build();
 app.UseCors();
 
-// "DB" en memoria: Lista de productos
 var listaProductos = new List<Producto>
 {
     new(1, "Camiseta básica", 12.99m, "Ropa", "https://picsum.photos/seed/prod1/400/300"),
@@ -27,20 +27,81 @@ var listaProductos = new List<Producto>
     new(10, "Sudadera con capucha", 34.95m, "Ropa", "https://picsum.photos/seed/prod10/400/300"),
 };
 
-// GET /
-app.MapGet("/", () => Results.Ok("API funcionando ✅ Prueba /productos"));
+//Ruta que saluda
+app.MapGet("/", () => Results.Ok("ESTA TODO CORRECTO"));
 
-// GET /productos
+// ruta para obtener todos los productos
 app.MapGet("/productos", () => Results.Ok(listaProductos));
 
-// GET /productos/{id}
+// ruta para obtener un producto por id
 app.MapGet("/productos/{id:int}", (int id) =>
 {
-    var producto = listaProductos.FirstOrDefault(p => p.Id == id);
-    return producto is null ? Results.NotFound() : Results.Ok(producto);
+    var producto = listaProductos.FirstOrDefault(elementoDeLista => elementoDeLista.Id == id);
+    return producto is null ? Results.NotFound("404, Producto no encontrado") : Results.Ok(producto);
+
+});
+
+app.MapPost("/productos", (ProductoCrear nuevoProducto) =>
+{
+    if (string.IsNullOrWhiteSpace(nuevoProducto.Nombre)) 
+    {
+        return Results.BadRequest("dale un nombre que no tiene");
+    }
+    if (nuevoProducto.Precio <= 0) 
+    {
+        return Results.BadRequest("el precio debe ser mayor a 0");
+        
+        
+    }
+    //var idProducto = listaProductos.Any() ? listaProductos.Max(elementoLista => elementoLista.Id) + 1 : 1;
+    
+    var idProducto = 1;// si no tiene nada no entra a la condicion siguiente y se queda en 1
+    if (listaProductos.Any())// evaluamos si tiene elementos true/false
+    {
+        idProducto = listaProductos.Max(elementoDeLista => elementoDeLista.Id) + 1;
+    }
+    
+    // crear el producto
+    var productoCreado = new Producto(
+        idProducto,
+        nuevoProducto.Nombre,
+        nuevoProducto.Precio, 
+        nuevoProducto.Categoria, 
+        nuevoProducto.UrlImagen
+    );
+    
+    //lo damos de alta en la listaProductos
+    listaProductos.Add(productoCreado);
+    return Results.Created("si se creo el producto ", productoCreado);
+});
+
+app.MapPut("/productos", (Producto productoActualizado) =>
+{
+    // verificamos si el producto existe
+    if (listaProductos.FirstOrDefault(elementoLista => elementoLista.Id == productoActualizado.Id) == null)
+    {
+        return Results.NotFound("No se encontro el producto para actualizar");
+    }
+    //sacamos el id del producto a actualizar
+    var index = listaProductos.FindIndex(elementoLista => elementoLista.Id == productoActualizado.Id);
+    // cambiamos el producto en la lista por el que nos esta llegando
+    listaProductos[index] = productoActualizado;
+    return Results.Ok("Producto actualizado correctamente", productoActualizado);
+});
+
+app.MapDelete("/productos/{id:int}", (int id) =>
+{
+    var producto = listaProductos.FirstOrDefault(elementoDeLista => elementoDeLista.Id == id);
+    if (producto == null)
+    {
+        return Results.NotFound("No se encontro el producto para eliminar");
+    }
+    listaProductos.Remove(producto);
+    return Results.Ok("Producto eliminado correctamente", producto);
 });
 
 app.Run();
 
-// Modelos
 public record Producto(int Id, string Nombre, decimal Precio, string Categoria, string UrlImagen);
+
+public record ProductoCrear(string Nombre, decimal Precio, string Categoria, string UrlImagen);
--- ConsumirApi/api_productos/obj/Debug/net10.0/api_productos.AssemblyInfo.cs
+++ ConsumirApi/api_productos/obj/Debug/net10.0/api_productos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api_productos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95326a19c90d2fecca073dbe3a26e8aaaacc7aa9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55c21795852bedba7cf65dc74755470669218422")]
 [assembly: System.Reflection.AssemblyProductAttribute("api_productos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api_productos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
